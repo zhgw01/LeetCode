3768591cd092df00564cfc20f9c960341f996ab8
Gongwei Zhang <zhgw01@gmail.com>
2013-04-22 21:25:34 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Zhang Gongwei on 4/22/13.
+//  Copyright (c) 2013 Zhang Gongwei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
